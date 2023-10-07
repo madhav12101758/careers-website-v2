f94425c3a2bc1a9c5aa211d31c5bcd1f5510810b
madhav12101758 <108966928+madhav12101758@users.noreply.github.com>
2023-10-07 04:56:31 +0000

    db

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine,text
+import os
 
-db_connection_string="mysql+pymysql://umhcv3eetm3r68ur393v:pscale_pw_lf8zjVcqjfxNCXrRN0soSg0xGNC2Re7CmHU8Vc3DC6X@aws.connect.psdb.cloud/oasis?charset=utf8mb4"
+db_connection_string="mysql+pymysql://7w4c0lf1nb6kynz9zb2d:pscale_pw_9LbadRAHNsquIBMTaUygmCULY3b6R4mO7GNBmPLbYzQ@aws.connect.psdb.cloud/oasis?charset=utf8mb4"
 engine = create_engine(db_connection_string,connect_args={
         "ssl": {
             "ssl_ca": "ca.pem",
@@ -8,6 +9,8 @@ engine = create_engine(db_connection_string,connect_args={
             "ssl_key": "client-key.pem"
         }
     })
+
+
 
 def get_jobs_from_db():
   with engine.connect() as conn:
